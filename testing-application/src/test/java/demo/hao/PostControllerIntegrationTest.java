71edfa8351cf0e4ceef45370c628c99681e4d469
hao <hao.deng.m@gmail.com>
2021-08-13 23:03:29 +0200

    add using JsonPath to test

--- testing-application/src/test/java/demo/hao/PostControllerIntegrationTest.java
+++ testing-application/src/test/java/demo/hao/PostControllerIntegrationTest.java
@@ -1,12 +1,14 @@
 package demo.hao;
 
-import org.junit.jupiter.api.Assertions;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(classes = {TestDemoApplication.class},
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -25,7 +27,13 @@ class PostControllerIntegrationTest {
                         String.class);
 
         String expected = "[{\"id\":1,\"name\":\"test 1\"},{\"id\":2,\"name\":\"test 2\"},{\"id\":3,\"name\":\"test 3\"}]";
-        Assertions.assertEquals(expected, response.getBody());
+        assertThat(response.getBody()).isEqualTo(expected);
+
+        // alternatively, use JsonPath test
+        Object dataObject = JsonPath.parse(response.getBody()).read("$[?(@.id == 2)]");
+        String dataString = dataObject.toString();
+        assertThat(dataString).contains("2");
+        assertThat(dataString).contains("test 2");
     }
 
     @Test
@@ -37,6 +45,6 @@ class PostControllerIntegrationTest {
                         String.class);
 
         String expected = "{\"id\":1,\"name\":\"test 1\"}";
-        Assertions.assertEquals(expected, response.getBody());
+        assertThat(response.getBody()).isEqualTo(expected);
     }
 }
